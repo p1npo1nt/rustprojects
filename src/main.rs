325b63288650972fa7e073e04ef44c195d319bc7
p1npo1nt <48339921+p1npo1nt@users.noreply.github.com>
2025-04-06 14:41:15 -0400

    guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
+//guessing game basic input/output
+//init mutable variable guess bound to an empty String instance
+//read input (read_line) into a reference of guess (&mut guess), ampersand means reference
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("Input guess: ");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
